8838cf190c0c73af63afcad241b15f8fb5846232
Derananer <10ilay1998@gmail.com>
2020-03-31 02:16:14 +0400

    updates

---
+++ src/company/programminPercolation/PPResult.java
@@ -0,0 +1,9 @@
+package company.programminPercolation;
+
+import company.entity.Cell;
+
+public class PPResult {
+    private Cell blackCell;
+    private Cell redCell;
+    private double distance;
+}
