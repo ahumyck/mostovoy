910dc56ef61d92ab32c039bcba8d47c949a891b2
Derananer <10ilay1998@gmail.com>
2020-04-08 19:12:14 +0400

    update

--- src/main/java/com/mostovoy_company/SpringBootExampleApplication.java
+++ src/main/java/com/mostovoy_company/SpringBootExampleApplication.java
@@ -1,17 +1,7 @@
 package com.mostovoy_company;
 
-import com.mostovoy_company.kafka.dto.Message;
-import com.mostovoy_company.kafka.dto.Response;
 import javafx.application.Application;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.kafka.support.converter.DefaultJackson2JavaTypeMapper;
-import org.springframework.kafka.support.converter.Jackson2JavaTypeMapper;
-import org.springframework.kafka.support.converter.RecordMessageConverter;
-import org.springframework.kafka.support.converter.StringJsonMessageConverter;
-
-import java.util.HashMap;
-import java.util.Map;
 
 @SpringBootApplication
 public class SpringBootExampleApplication {
--- src/main/java/com/mostovoy_company/kafka/KafkaConsumerConfig.java
+++ src/main/java/com/mostovoy_company/kafka/KafkaConsumerConfig.java
@@ -58,7 +58,7 @@ public class KafkaConsumerConfig {
 
     @Bean
     public ConsumerFactory<Long, Response> responseConsumerFactory() {
-        return new DefaultKafkaConsumerFactory<>(consumerConfigs());
+        return new DefaultKafkaConsumerFactory<>(responseConfigs());
     }
 
 
@@ -68,7 +68,20 @@ public class KafkaConsumerConfig {
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaServer);
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, LongDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, "group1");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, kafkaGroupId);
+        props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true);
+        props.put(ConsumerConfig.MAX_POLL_INTERVAL_MS_CONFIG, Integer.MAX_VALUE);
+        props.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, 1);
+        return props;
+    }
+
+    @Bean
+    public Map<String, Object> responseConfigs() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaServer);
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, LongDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "group2");
         props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true);
         props.put(ConsumerConfig.MAX_POLL_INTERVAL_MS_CONFIG, Integer.MAX_VALUE);
         props.put(ConsumerConfig.MAX_POLL_RECORDS_CONFIG, 1);
