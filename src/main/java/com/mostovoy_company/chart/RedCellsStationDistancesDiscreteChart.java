c78453d1c37c44e279e431f191a13c5bfb628ad8
ahumyck <ahumyckbf4@gmail.com>
2020-04-14 21:00:07 +0400

    renaming

--- src/main/java/com/mostovoy_company/chart/RedCellsStationDistancesDiscreteChart.java
+++ src/main/java/com/mostovoy_company/chart/RedCellsStationDistancesDiscreteChart.java
@@ -6,8 +6,8 @@ import org.springframework.stereotype.Component;
 import static com.mostovoy_company.chart.ChartNames.RED_CELLS_STATION_DISTANCES_NE_PI_CHART;
 
 @Component(RED_CELLS_STATION_DISTANCES_NE_PI_CHART)
-public class RedCellsStationDistancesNePiChart extends BaseLineChartData {
-    public RedCellsStationDistancesNePiChart(Painter painter) {
+public class RedCellsStationDistancesDiscreteChart extends BaseLineChartData {
+    public RedCellsStationDistancesDiscreteChart(Painter painter) {
         super(painter);
     }
 }
--- src/main/java/com/mostovoy_company/chart/RedCellsStationDistancesPythagorasChart.java
+++ src/main/java/com/mostovoy_company/chart/RedCellsStationDistancesPythagorasChart.java
@@ -6,8 +6,8 @@ import org.springframework.stereotype.Component;
 import static com.mostovoy_company.chart.ChartNames.RED_CELLS_STATION_DISTANCES_PI_CHART;
 
 @Component(RED_CELLS_STATION_DISTANCES_PI_CHART)
-public class RedCellsStationDistancesPiChart extends BaseLineChartData {
-    public RedCellsStationDistancesPiChart(Painter painter) {
+public class RedCellsStationDistancesPythagorasChart extends BaseLineChartData {
+    public RedCellsStationDistancesPythagorasChart(Painter painter) {
         super(painter);
     }
 }
