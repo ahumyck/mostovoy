4f6975849b4745f11f44bd2a4b9d9aaadb1f0e25
ahumyck <ahumyckbf4@gmail.com>
2020-04-27 22:05:58 +0400

    code minor upd

--- src/main/java/com/mostovoy_company/expirement/chart_experiment/lightningbolt/neighborhood/DiagonalRules.java
+++ src/main/java/com/mostovoy_company/expirement/chart_experiment/lightningbolt/neighborhood/DiagonalRules.java
@@ -11,10 +11,17 @@ public class DiagonalRules implements NeighborhoodRules {
     @Override
     public List<Paired<Integer, Integer>> getRules(int i, int j) {
         List<Paired<Integer,Integer>> rules = new ArrayList<>();
-        rules.add(new Paired<>(i + 1, j + 1)); //right and up
-        rules.add(new Paired<>(i + 1, j - 1)); //right and down
-        rules.add(new Paired<>(i - 1, j + 1)); //left and up
-        rules.add(new Paired<>(i - 1, j - 1)); //left and down
+//        rules.add(new Paired<>(i + 1, j + 1)); //right and up
+//        rules.add(new Paired<>(i + 1, j - 1)); //right and down
+//        rules.add(new Paired<>(i - 1, j + 1)); //left and up
+//        rules.add(new Paired<>(i - 1, j - 1)); //left and down
+        rules.add(new Paired<>(i - 1, j)); //up
+        rules.add(new Paired<>(i + 1, j)); //down
+        rules.add(new Paired<>(i, j + 1)); //right
+        rules.add(new Paired<>(i, j - 1)); //left
+
+        rules.add(new Paired<>(i + 1, j + 1)); //down and right
+        rules.add(new Paired<>(i + 1, j - 1)); //down and left
         return rules;
     }
 }
