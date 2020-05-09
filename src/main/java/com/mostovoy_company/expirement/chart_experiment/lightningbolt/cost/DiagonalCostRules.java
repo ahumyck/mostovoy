1a914da143dfe34581a01c223a9385390fd8bc14
ahumyck <ahumyckbf4@gmail.com>
2020-05-10 00:06:24 +0400

    P U N I S H M E N T

--- src/main/java/com/mostovoy_company/expirement/chart_experiment/lightningbolt/cost/DiagonalCostRules.java
+++ src/main/java/com/mostovoy_company/expirement/chart_experiment/lightningbolt/cost/DiagonalCostRules.java
@@ -6,39 +6,30 @@ import org.springframework.stereotype.Component;
 @Component
 public class DiagonalCostRules extends CostRules {
 
+    private static int INFINITY = Integer.MAX_VALUE;
+
     @Override
     public int setCostUsingRules(Cell startCell, Cell endCell, int shiftedSize) {
         int cheapCost = 1;
-        int expensiveCost = shiftedSize * shiftedSize + 1;
-        int punishCost = expensiveCost * expensiveCost;
+        int expensiveCost = shiftedSize + 1;
+
+        int cheapPunish = expensiveCost * expensiveCost;
+        int expensivePunish = expensiveCost * cheapPunish;
 
         int dx = endCell.getX() - startCell.getX();
         int dy = endCell.getY() - startCell.getY();
 
         if (dx == 0) {
-            //if we are here, it means dy != 0
-            if (dy < 0) {
-                //if dy > 0 it means start cell is above end cell
-                //and its fine
-                return this.setCosts(startCell, endCell, cheapCost, cheapCost);
-            } //dy == 0 and dx == 0 is not possible
-            else {
-                //if dy < 0 it means we're looking backwards
-                //awful situation, DO NOT REWARD IT
-                return this.setCosts(startCell, endCell, punishCost, punishCost);
+            if (dy > 0) {
+                return this.setCosts(startCell, endCell, cheapCost, expensiveCost);
+            } else {
+                return this.setCosts(startCell, endCell, cheapPunish, expensivePunish);
             }
         }
-
         if (dy == 0) {
-            //if we are here, it means dy != 0
             if (dx < 0) {
-                //if dx > 0 it means start cell is to the right of end cell
-                //and it's awful situation, PUNISH IT
-                return this.setCosts(startCell, endCell, punishCost, punishCost*punishCost);
-            } //dy == 0 and dx == 0 is not possible
-            else {
-                //if dx < 0 it means we're looking towards
-                //and its fine
+                return this.setCosts(startCell, endCell, cheapPunish, expensivePunish);
+            } else {
                 return this.setCosts(startCell, endCell, cheapCost, expensiveCost);
             }
         }
